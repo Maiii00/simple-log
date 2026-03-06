a52f2fe654a3ec014cf68ff149806ccc3f34699e
Maiii00 <maykrop0717@gmail.com>
2026-03-06 17:58:19 +0800

    fix autoLog

--- simple-log/src/main/java/io/github/maiii00/simple_log/aspect/LogAspect.java
+++ simple-log/src/main/java/io/github/maiii00/simple_log/aspect/LogAspect.java
@@ -3,6 +3,7 @@ package io.github.maiii00.simple_log.aspect;
 import io.github.maiii00.simple_log.annotation.AutoLog;
 import io.github.maiii00.simple_log.model.LogData;
 import io.github.maiii00.simple_log.service.LogStorageService;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -13,6 +14,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Aspect
+@Slf4j
 public class LogAspect {
     private final LogStorageService storageService;
 
@@ -50,6 +52,15 @@ public class LogAspect {
                     .success(isSuccess)
                     .errorMsg(errorMsg)
                     .build();
+
+            String status = logData.isSuccess() ? "SUCCESS" : "FAILED";
+            if (isSuccess) {
+                log.info("[Simple-Log] [{}] Method: {} | Time: {}ms | Args: {}",
+                        status, logData.getMethodName(), logData.getExecutionTime(), logData.getArgs());
+            } else {
+                log.error("[Simple-Log] [{}] Method: {} | Time: {}ms | Args: {}",
+                        status, logData.getMethodName(), logData.getExecutionTime(), logData.getArgs());
+            }
 
             try {
                 storageService.save(logData);
